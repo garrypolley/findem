cf7e965f18447674a84ce74a16f121fa92392d05
Garry Polley <garrympolley@gmail.com>
2020-07-12 20:04:07 -0500

    Add buttons for driving and walking

--- src/templates/alert-post.js
+++ src/templates/alert-post.js
@@ -6,6 +6,85 @@ import Layout from '../components/Layout'
 import { HTMLContent } from '../components/Content'
 
 
+function mydrive() {
+    navigator.geolocation.getCurrentPosition(doDrivingDirections);
+}
+
+function mywalk() {
+    navigator.geolocation.getCurrentPosition(doWalkingDirections);
+}
+
+function doDrivingDirections(position) {
+    var lat = position.coords.latitude
+    var long = position.coords.longitude
+    if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPad/i)) || (navigator.userAgent.match(/Mac/i))) {
+        var sites = [
+            "http://maps.apple.com/?daddr=" + (lat + (-.03 * (Math.random()))) + "," + (long + (-.03 * (Math.random()))) + "&dirflg=d",
+            "http://maps.apple.com/?daddr=" + (lat + (.03 * (Math.random()))) + "," + (long + (.03 * (Math.random()))) + "&dirflg=d",
+            "http://maps.apple.com/?daddr=" + (lat + (-.03 * (Math.random()))) + "," + (long + (.03 * (Math.random()))) + "&dirflg=d",
+            "http://maps.apple.com/?daddr=" + (lat + (.03 * (Math.random()))) + "," + (long + (-.03 * (Math.random()))) + "&dirflg=d",
+            "http://maps.apple.com/?daddr=gas+station&dirflg=d"];
+        window.location.assign(sites[Math.floor(Math.random() * sites.length)])
+    }
+    else {
+        var sites = [
+            "https://www.google.com/maps/dir/?api=1&destination=" + lat + "," + long + "&travelmode=driving&dir_action=navigate&waypoints=" + (lat + (-.01 * (Math.random()))) + "," + (long + (.01 * (Math.random()))) + "%7C" + (lat + (.01 * (Math.random()))) + "," + (long + (-.01 * (Math.random()))),
+            "https://www.google.com/maps/dir/?api=1&destination=" + lat + "," + long + "&travelmode=driving&dir_action=navigate&waypoints=" + (lat + (.01 * (Math.random()))) + "," + (long + (-.01 * (Math.random()))) + "%7C" + (lat + (-.01 * (Math.random()))) + "," + (long + (.01 * (Math.random()))),
+            "https://www.google.com/maps/dir/?api=1&destination=" + lat + "," + long + "&travelmode=driving&dir_action=navigate&waypoints=" + (lat + (-.01 * (Math.random()))) + "," + (long + (-.01 * (Math.random()))) + "%7C" + (lat + (.01 * (Math.random()))) + "," + (long + (.01 * (Math.random()))),
+            "https://www.google.com/maps/dir/?api=1&destination=" + lat + "," + long + "&travelmode=driving&dir_action=navigate&waypoints=" + (lat + (.01 * (Math.random()))) + "," + (long + (.01 * (Math.random()))) + "%7C" + (lat + (-.01 * (Math.random()))) + "," + (long + (-.01 * (Math.random()))),
+            "https://www.google.com/maps/dir/?api=1&destination=" + lat + "," + long + "&travelmode=driving&dir_action=navigate&waypoints=gas+station%7C" + (lat + (.01 * (Math.random()))) + "," + (long + (-.01 * (Math.random())))
+        ];
+        window.open(sites[Math.floor(Math.random() * sites.length)])
+    }
+}
+
+function doWalkingDirections(position) {
+    var lat = position.coords.latitude
+    var long = position.coords.longitude
+    if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPad/i)) || (navigator.userAgent.match(/Mac/i))) {
+        var sites = [
+            "http://maps.apple.com/?dirflg=w&daddr=" + (lat + (-.004 * (Math.random()))) + "," + (long + (-.004 * (Math.random()))),
+            "http://maps.apple.com/?dirflg=w&daddr=" + (lat + (.004 * (Math.random()))) + "," + (long + (.004 * (Math.random()))),
+            "http://maps.apple.com/?dirflg=w&daddr=" + (lat + (-.004 * (Math.random()))) + "," + (long + (.004 * (Math.random()))),
+            "http://maps.apple.com/?dirflg=w&daddr=" + (lat + (.004 * (Math.random()))) + "," + (long + (-.004 * (Math.random())))]
+        window.location.assign(sites[Math.floor(Math.random() * sites.length)])
+    }
+    else {
+        var sites = [
+            "https://www.google.com/maps/dir/?api=1&destination=" + lat + "," + long + "&travelmode=walking&dir_action=navigate&waypoints=" + (lat + (-.004 * (Math.random()))) + "," + (long + (.004 * (Math.random()))) + "%7C" + (lat + (.004 * (Math.random()))) + "," + (long + (-.004 * (Math.random()))),
+            "https://www.google.com/maps/dir/?api=1&destination=" + lat + "," + long + "&travelmode=walking&dir_action=navigate&waypoints=" + (lat + (.004 * (Math.random()))) + "," + (long + (-.004 * (Math.random()))) + "%7C" + (lat + (-.004 * (Math.random()))) + "," + (long + (.004 * (Math.random()))),
+            "https://www.google.com/maps/dir/?api=1&destination=" + lat + "," + long + "&travelmode=walking&dir_action=navigate&waypoints=" + (lat + (-.004 * (Math.random()))) + "," + (long + (-.004 * (Math.random()))) + "%7C" + (lat + (.004 * (Math.random()))) + "," + (long + (.004 * (Math.random()))),
+            "https://www.google.com/maps/dir/?api=1&destination=" + lat + "," + long + "&travelmode=walking&dir_action=navigate&waypoints=" + (lat + (.004 * (Math.random()))) + "," + (long + (.004 * (Math.random()))) + "%7C" + (lat + (-.004 * (Math.random()))) + "," + (long + (-.004 * (Math.random())))
+        ];
+        window.open(sites[Math.floor(Math.random() * sites.length)])
+    }
+}
+
+function showTransPosition(position) {
+    var lat = position.coords.latitude
+    var long = position.coords.longitude
+    if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPad/i))) {
+        var sites = [
+            "http://maps.apple.com/?daddr=" + (lat + (-.1 * (Math.random()))) + "," + (long + (-.05 * (Math.random()))) + "&dirflg=r",
+            "http://maps.apple.com/?daddr=" + (lat + (.1 * (Math.random()))) + "," + (long + (.05 * (Math.random()))) + "&dirflg=r",
+            "http://maps.apple.com/?daddr=" + (lat + (-.1 * (Math.random()))) + "," + (long + (.05 * (Math.random()))) + "&dirflg=r",
+            "http://maps.apple.com/?daddr=" + (lat + (.1 * (Math.random()))) + "," + (long + (-.05 * (Math.random()))) + "&dirflg=r"];
+        window.location.assign(sites[Math.floor(Math.random() * sites.length)])
+    }
+    else {
+        var sites = [
+            "https://www.google.com/maps/dir/?api=1&origin=" + lat + "," + long + "&destination=" + (lat + (-.1 * (Math.random()))) + "," + (long + (-.05 * (Math.random()))) + "&travelmode=transit",
+            "https://www.google.com/maps/dir/?api=1&origin=" + lat + "," + long + "&destination=" + (lat + (-.1 * (Math.random()))) + "," + (long + (-.05 * (Math.random()))) + "&travelmode=transit",
+            "https://www.google.com/maps/dir/?api=1&origin=" + lat + "," + long + "&destination=" + (lat + (-.1 * (Math.random()))) + "," + (long + (-.05 * (Math.random()))) + "&travelmode=transit",
+            "https://www.google.com/maps/dir/?api=1&origin=" + lat + "," + long + "&destination=" + (lat + (-.1 * (Math.random()))) + "," + (long + (-.05 * (Math.random()))) + "&travelmode=transit",
+        ];
+        window.open(sites[Math.floor(Math.random() * sites.length)])
+    }
+}
+
+
+
+
 export const AlertPostTemplate = ({
   name,
   description,
@@ -32,6 +111,8 @@ export const AlertPostTemplate = ({
             </h1>
             <img src={!!mainImage.childImageSharp ? mainImage.childImageSharp.fluid.src : mainImage}></img>
             <p>{description}</p>
+            <button onClick={mydrive}>Drive</button>
+            <button onClick={mywalk}>Walk</button>
           </div>
         </div>
       </div>
